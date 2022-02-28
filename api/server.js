949865dbfd13c44db99d345a5db8e4b5488596a2
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-28 16:50:55 -0600

    created users model

--- api/server.js
+++ api/server.js
@@ -1,28 +1,15 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const db = require('./data/db-config')
-
-function getAllUsers() { return db('users') }
-
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
 
 const server = express()
+
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
-
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
+server.use("/", (req, res) => {
+  res.send("African Marketplace Backend")
 })
 
 module.exports = server
